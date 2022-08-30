75cdaaf8a7852add08cf292ffbe1701ea20ac59a
Lucasvgb <lucasvgb@id.uff.br>
2022-08-29 21:02:17 -0300

    primeiro commit

---
+++ Novos_projetos_repositorios/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Novos_projetos_repositorios
+    {
+    internal class Program
+        {
+        static void Main(string [] args)
+            {
+            Console.WriteLine("Entre com seu nome completo");
+            Console.ReadLine();
+            Console.WriteLine("Entre com o preço de um produto");
+            Console.ReadLine();
+            Console.WriteLine("Entre com seu último nome,idade e altura");
+            string n = Console.ReadLine();
+            string [] vet = n.Split(' ');
+            string p1 = vet[0];
+            int p2 = int.Parse(vet[1]);
+            double p3 = double.Parse(vet [2]);
+            Console.WriteLine(n);
+            Console.ReadLine();
+            }
+
+        }
+    }
---
+++ Novos_projetos_repositorios/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("Novos_projetos_repositorios")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Novos_projetos_repositorios")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("765bf752-22f3-4a01-85be-75098c16816c")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
